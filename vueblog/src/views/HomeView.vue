53829b3d1dc45d830505e1bdf053cd4b953d82bb
Jehan_Tuf <jehan.adhan01@gmail.com>
2026-02-22 12:11:07 +0700

    setup project dan cara kerja compositioning api

---
+++ vueblog/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Nama saya {{ name }}</p>
+    <p>Number saya {{ number }}</p>
+  </div>
+</template>
+
+  <script >
+  export default {
+    name: 'HomeView',
+    // function yang akan di jalankan pertama kali ketika component di buat
+    setup() {
+    // data yang akan di gunakan di template
+    const name = "jehan"
+    const number = 132324234
+    // tidak perlu menggunakan key dan value jika nama variabel sama dengan nama key
+    return {
+      name,
+      number}
+    }, 
+  }
+  </script>
