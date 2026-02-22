a0ebee8d154d63103df6f29b034bacd2b6713eb5
Jehan_Tuf <jehan.adhan01@gmail.com>
2026-02-23 05:42:12 +0700

    membuat db.json atau fake api, melakukan fetch data menggunakan async await

--- vueblog/src/views/HomeView.vue
+++ vueblog/src/views/HomeView.vue
@@ -4,6 +4,8 @@
     <h1>Home</h1>
     <!-- #3 mengirim data posts kedalam props :dataposts -->
     <PostList :dataposts="posts" v-if="showPost" />
+    <!-- jika terjadi error, maka akan muncul pesan dari throw Error -->
+    <div v-if="error">{{ error }}</div>
   </div>
   <!-- ini button untuk mentriger mounted dan unmounted dengan mengubah value showpost, saya tambakan tenari option agar lebih bervariatif saja -->
   <button @click="showPost = !showPost">{{ showPost ? 'Hide Posts' : 'Show Posts' }}</button>
@@ -23,25 +25,30 @@
     // function yang akan di jalankan pertama kali ketika component di buat
     setup() {
       // #1 data dummy untuk post, nanti akan kita ganti dengan data dari firebase
-      const posts = ref([
-        {
-          id: 1,
-          title: "Belajar Vue JS",
-          content: "Vue JS adalah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (user interface) yang interaktif dan responsif. Vue JS memiliki sintaks yang sederhana dan mudah dipahami, sehingga cocok untuk pemula yang ingin belajar tentang pengembangan web.",
-        },
-        {
-          id: 2,
-          title: "Belajar React JS",
-          content: "React JS adalah library JavaScript yang digunakan untuk membangun antarmuka pengguna (user interface) yang interaktif dan responsif. React JS dikembangkan oleh Facebook dan memiliki sintaks yang mirip dengan HTML, sehingga mudah dipahami oleh pengembang web.",
-        },
-      ]);  
+      const posts = ref([]); 
+      const error = ref(null); 
 
+      const load = async () => {
+        try {
+          // data di ubah menjadi fetch dari firebase, pastikan sudah menjalankan firebase menggunakan dummy data db.json
+          let response = await fetch('http://localhost:3000/posts'); 
+          if (!response.ok) {
+          throw new Error('Failed to fetch posts'); 
+        } 
+          const data = await response.json(); 
+          posts.value = data; 
+        } catch (err) {
+          error.value = err.message
+          }
+      }
+
+      load();
       const showPost = ref(true);
 
       return {
         // #2 mengembalikan data yang akan digunakan di template
         posts,
-        showPost
+        showPost, error,
       }
     }, 
   }
