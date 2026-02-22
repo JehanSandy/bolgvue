46066ac30c3f36a95e27df9e6cb91b412733f701
Jehan_Tuf <jehan.adhan01@gmail.com>
2026-02-22 14:57:19 +0700

    ref vs reacktive

--- vueblog/src/views/HomeView.vue
+++ vueblog/src/views/HomeView.vue
@@ -1,39 +1,61 @@
 
 <template>
   <div>
-    <h1>Home</h1>
-    <p ref="p">Nama saya {{ name }}</p>
-    <p ref="p2">Number saya {{ number }}</p>
+    <h1>ref</h1>
+    <p >Nama saya {{ contactOne.name }}</p>
+    <p >Number saya {{ contactOne.number }}</p>
   </div>
   <button @click="handleClick">Click Me</button> <br/>
-
   <!-- ini menambah number menggunakan increment mengunakan function ++ (number++) -->
-  <button @click="number++">increment</button>
-  <input type="text" v-model="name" />
+  <button @click="contactOne.number++">increment</button>
+  <input type="text" v-model="contactOne.name" />
+
+  <div>
+    <h1>Reaktive</h1>
+    <p >Nama saya {{ contactTwo.name }}</p>
+    <p >Number saya {{ contactTwo.number }}</p>
+  </div>
+  <button @click="handleClicktwo">Click Me</button> <br/>
+  <!-- ini menambah number menggunakan increment mengunakan function ++ (number++) -->
+  <button @click="contactTwo.number++">increment</button>
+  <input type="text" v-model="contactTwo.name" />
+
+
 </template>
 
   <script >
-  import { ref } from "vue";
+  // ref dan reactive harus di inport disini
+  import { ref, reactive } from "vue";
   export default {
     name: 'HomeView',
     // function yang akan di jalankan pertama kali ketika component di buat
     setup() {
+    const contactOne = ref({
+      name: "jehan",
+      number: 123489432487
+    })
 
-    const p = ref(null)
-    const p2 = ref(null)
-    // ini cara untuk membuat reactive variable, jika kita ingin membuat variable yang bisa berubah-ubah, kita harus menggunakan ref, jika tidak maka variable tersebut tidak akan bisa berubah-ubah
-    const name = ref("jehan")
-    const number = ref(132324234)
+    const contactTwo = reactive({
+      name: "hendro kartiko",
+      number: 10893748192828
+    })
     
+    // ini menggunakan ref untuk mengubah value di html
     const handleClick = () => {
-      name.value = "rehan"
-      number.value = 123489432487
+      contactOne.value.name = "bukan jehan"
+      contactOne.value.number = 1010199432487
     }
+    // ini menggunakan reactive untuk mengubah value di html, perbedaannya ada di syntaxnya, jika menggunakan ref kita harus menggunakan .value untuk mengakses value dari variable tersebut, sedangkan jika menggunakan reactive kita tidak perlu menggunakan .value untuk mengakses value dari variable tersebut.
+    const handleClicktwo = () => {
+      contactTwo.name = "kartika sari"
+      contactTwo.number = 1123213
+    }
+    // yang di pakai nanti tetap ref karna didalam reactive memiliki kelemahan tidak bisa mengubah bentuk data primitif
+
     
     // tidak perlu menggunakan key dan value jika nama variabel sama dengan nama key
     return {
-      name,
-      number, handleClick}
+      handleClick, contactOne, contactTwo, handleClicktwo}
     }, 
   }
   </script>
