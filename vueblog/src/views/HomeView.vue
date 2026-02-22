7cab4df03ae4711f4a5e27aac9435a44b8cc4a46
Jehan_Tuf <jehan.adhan01@gmail.com>
2026-02-22 15:36:27 +0700

    manipulasi data menggunakan compute studi kasus filter nama

--- vueblog/src/views/HomeView.vue
+++ vueblog/src/views/HomeView.vue
@@ -1,61 +1,45 @@
 
 <template>
-  <div>
-    <h1>ref</h1>
-    <p >Nama saya {{ contactOne.name }}</p>
-    <p >Number saya {{ contactOne.number }}</p>
+  <div class="home">
+    <h1>Home</h1>
+    <!-- perulanga disini sudah tidak melakukan looping names, namun looping data yg telah di tampung pada filteredNames -->
+    <div v-for="(name, index) in filteredNames" :key="index">
+      <p>{{ name }}</p>
+    </div>
+    <input type="text" v-model="search" />
   </div>
-  <button @click="handleClick">Click Me</button> <br/>
-  <!-- ini menambah number menggunakan increment mengunakan function ++ (number++) -->
-  <button @click="contactOne.number++">increment</button>
-  <input type="text" v-model="contactOne.name" />
+  
 
-  <div>
-    <h1>Reaktive</h1>
-    <p >Nama saya {{ contactTwo.name }}</p>
-    <p >Number saya {{ contactTwo.number }}</p>
-  </div>
-  <button @click="handleClicktwo">Click Me</button> <br/>
-  <!-- ini menambah number menggunakan increment mengunakan function ++ (number++) -->
-  <button @click="contactTwo.number++">increment</button>
-  <input type="text" v-model="contactTwo.name" />
+  
 
 
 </template>
 
   <script >
   // ref dan reactive harus di inport disini
-  import { ref, reactive } from "vue";
+  import { ref, reactive, computed } from "vue";
   export default {
     name: 'HomeView',
     // function yang akan di jalankan pertama kali ketika component di buat
     setup() {
-    const contactOne = ref({
-      name: "jehan",
-      number: 123489432487
-    })
+      // seach ini menampung data yang di inputkan pada input, dan names ini menampung data nama yang akan di looping dan di proses untuk di filter
+      const search = ref('');
+      const names = ref([
+        'Jeihan',
+        'Rizky',
+        'Fajar',
+        'Dimas',
+        'Rizal','joko','eko','budi','siti','susan', 'siti','henry','james','john','doe','smith'
+      ]);
 
-    const contactTwo = reactive({
-      name: "hendro kartiko",
-      number: 10893748192828
-    })
-    
-    // ini menggunakan ref untuk mengubah value di html
-    const handleClick = () => {
-      contactOne.value.name = "bukan jehan"
-      contactOne.value.number = 1010199432487
-    }
-    // ini menggunakan reactive untuk mengubah value di html, perbedaannya ada di syntaxnya, jika menggunakan ref kita harus menggunakan .value untuk mengakses value dari variable tersebut, sedangkan jika menggunakan reactive kita tidak perlu menggunakan .value untuk mengakses value dari variable tersebut.
-    const handleClicktwo = () => {
-      contactTwo.name = "kartika sari"
-      contactTwo.number = 1123213
-    }
-    // yang di pakai nanti tetap ref karna didalam reactive memiliki kelemahan tidak bisa mengubah bentuk data primitif
+      // filter menggantikan names, dengan menampung nama yang sudah di filter sesuai dengan search
+      const filteredNames = computed(() => {
+        return names.value.filter(name => name.toLowerCase().includes(search.value.toLowerCase()));
+      });
 
-    
-    // tidak perlu menggunakan key dan value jika nama variabel sama dengan nama key
-    return {
-      handleClick, contactOne, contactTwo, handleClicktwo}
+      return {
+      names, search, filteredNames
+      }
     }, 
   }
   </script>
